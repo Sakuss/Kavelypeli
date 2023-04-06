9461acc0a4699e5dbab4bb8143d16a1e36fa72ad
tpoldsam <teea.poldsam@gmail.com>
2023-04-06 13:41:49 +0300

    tidy up the code

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -12,63 +12,17 @@ class ShopPage extends StatefulWidget {
   State<ShopPage> createState() => _ShopPageState();
 }
 
-class ItemClass {
-  int _id;
-  String image;
-  int ingame_currency_price;
-  int money_price;
-  String name;
-
-  ItemClass(this._id, this.image, this.ingame_currency_price, this.money_price,
-      this.name);
-
-  factory ItemClass.fromJson(dynamic json) {
-    return ItemClass(
-        json['_id'] as int,
-        json['image'] as String,
-        json['ingame_currency_price'] as int,
-        json['money_price'] as int,
-        json['name'] as String);
-  }
-
-  @override
-  String toString() {
-    return '{ ${this._id}, ${this.image}, ${this.ingame_currency_price}, ${this.money_price}, ${this.name} }';
-  }
-}
-
 class _ShopPageState extends State<ShopPage> {
   final db = FirebaseFirestore.instance;
 
-  Future<List> getItems() async {
-    // Täällä pitäisi saada haettua: itemin nimi, kuva,
-    // ingamecurrency hinta ja oikeerahahinta
-    var itemId = 1;
-    var items = [];
-
-    db.collection("items").get().then(
-      (querySnapshot) {
-        print("Items fetched successfully");
-        for (var docSnapshot in querySnapshot.docs) {
-          items.add('${docSnapshot.id} => ${docSnapshot.data()}');
-        }
-        print(items);
-      },
-      onError: (e) => print("Error completing: $e"),
-    );
-
-    return items;
-  }
-
   void buyItem() {
-    // täällä pitäisi lisätä ostettu item käyttäjän
-    // inventoryyn ja tarvittaessa poistaa ingame rahaa
+    // nothing here yet
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
-        physics: const BouncingScrollPhysics(), // ?
+        physics: const BouncingScrollPhysics(),
         slivers: <Widget>[
           SliverPadding(
               padding: const EdgeInsets.all(20),
@@ -77,21 +31,19 @@ class _ShopPageState extends State<ShopPage> {
                 mainAxisSpacing: 10,
                 crossAxisCount: 2,
                 children: <Widget>[
-                  for (int i = 0;
-                      i < 10; // tänne listan mitan verran toistoa (list.length) tai jtn
-                      i++)
+                  for (int i = 0; i < 10; i++)
                     Container(
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: NetworkImage(
-                                  "https://picsum.photos/250?image=$i"), // tänne kuva oikealla indexillä i
+                                  "https://picsum.photos/250?image=$i"),
                               fit: BoxFit.cover)),
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: <Widget>[
                             ElevatedButton(
-                              onPressed: getItems,
+                              onPressed: buyItem,
                               style: ButtonStyle(
                                   shape: MaterialStateProperty.all<
                                           RoundedRectangleBorder>(
@@ -100,7 +52,7 @@ class _ShopPageState extends State<ShopPage> {
                                     topLeft: Radius.circular(15.0),
                                     bottomLeft: Radius.circular(15.0)),
                               ))),
-                              child: const Text("100 @"), // tänne pelirahahinta
+                              child: const Text("100 @"),
                             ),
                             ElevatedButton(
                               onPressed: buyItem,
@@ -112,7 +64,7 @@ class _ShopPageState extends State<ShopPage> {
                                     topRight: Radius.circular(15.0),
                                     bottomRight: Radius.circular(15.0)),
                               ))),
-                              child: const Text("1 €"), // tänne oikearahahinta
+                              child: const Text("1 €"),
                             ),
                           ]),
                     ),
