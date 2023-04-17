c08f84a6e45a4f18c7ad91f8babd44e59a1b811c
Olli Petäjäniemi <petajaol@gmail.com>
2023-04-17 12:39:36 +0300

    implement logic to query more users when scrolled

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -12,30 +12,63 @@ class Leaderboard extends StatefulWidget {
 }
 
 class _LeaderboardState extends State<Leaderboard> {
+  late Query _query;
+  final List<DocumentSnapshot> _data = [];
+  final int _perPage = 10;
+  DocumentSnapshot? _lastDocument;
+  bool _isLoading = false;
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getMoreUsers();
+  }
+
+  Future<void> _getMoreUsers() async {
+    _query = FirebaseFirestore.instance.collection('users').orderBy('points', descending: true).limit(_perPage);
+    if (_isLoading) return;
+    setState(() {
+      _isLoading = true;
+    });
+    final snapshot = _lastDocument == null ? await _query.get() : await _query.startAfterDocument(_lastDocument!).get();
+    final documents = snapshot.docs;
+    setState(() {
+      _data.addAll(documents);
+      _lastDocument = documents.isNotEmpty ? documents.last : null;
+    });
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Leaderboard'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('users').orderBy('points', descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot data = snapshot.data!.docs[index];
-                return ListTile(
-                  title: Text(data['username']),
-                  subtitle: Text(data['points'].toString()),
-                );
-              },
+    return NotificationListener(
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _data.length,
+        itemBuilder: (context, index) {
+          if (_isLoading && index == _data.length - 1) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
           }
-          return Center(child: CircularProgressIndicator());
+          final data = _data[index];
+          return ListTile(
+            title: Text(data['username']),
+            subtitle: Text(data['points'].toString()),
+          );
         },
       ),
+      onNotification: (notification) {
+        if (notification is ScrollEndNotification &&
+            _scrollController.position.extentAfter == 0 &&
+            _lastDocument != null) {
+          _getMoreUsers();
+        }
+        return false;
+      },
     );
   }
 }
